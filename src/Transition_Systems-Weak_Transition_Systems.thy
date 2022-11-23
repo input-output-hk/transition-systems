4ce75039b47c85039e138bff90f9341cd5733908
Javier Díaz <javier.diaz@iohk.io>
2022-11-23 11:47:49 -0300

    Use the weak transition system instead of the mixed one

--- src/Transition_Systems-Weak_Transition_Systems.thy
+++ src/Transition_Systems-Weak_Transition_Systems.thy
@@ -287,17 +287,15 @@ qed
 lemma mutual_silent_weak_transitions_up_to_bisimilarity:
   assumes "p \<Rightarrow>\<lparr>\<tau>\<rparr> p'" and "p' \<approx> q" and "q \<Rightarrow>\<lparr>\<tau>\<rparr> q'" and "q' \<approx> p"
   shows "p \<approx> q"
-unfolding weak_bisimilarity_is_mixed_bisimilarity using assms
-proof (coinduction arbitrary: p q p' q' rule: mixed.symmetric_up_to_rule [where \<F> = "[\<asymp>]"])
+using assms
+proof (coinduction arbitrary: p q p' q' rule: weak.symmetric_up_to_rule [where \<F> = "[\<approx>]"])
   case (simulation \<alpha> s)
-  from \<open>p \<rightarrow>\<lparr>\<alpha>\<rparr> s\<close> and \<open>q \<Rightarrow>\<lparr>\<tau>\<rparr> q'\<close> and \<open>q' \<approx> p\<close> obtain s' where "q' \<Rightarrow>\<lparr>\<alpha>\<rparr> s'" and "s' \<approx> s"
-    using transition_in_weak_transition_rule
+  from \<open>p \<Rightarrow>\<lparr>\<alpha>\<rparr> s\<close> and \<open>q \<Rightarrow>\<lparr>\<tau>\<rparr> q'\<close> and \<open>q' \<approx> p\<close> obtain s' where "q' \<Rightarrow>\<lparr>\<alpha>\<rparr> s'" and "s' \<approx> s"
     by (blast elim: weak.bisimilarity.cases)
   with \<open>q \<Rightarrow>\<lparr>\<tau>\<rparr> q'\<close> have "q \<Rightarrow>\<lparr>\<alpha>\<rparr> s'"
     by (auto simp add: relcompp_assoc, blast intro: rtranclp_trans)
   with \<open>s' \<approx> s\<close> show ?case
-    unfolding weak_bisimilarity_is_mixed_bisimilarity
-    by (fastforce intro: mixed.bisimilarity_symmetry_rule)
+    by (fastforce intro: weak.bisimilarity_symmetry_rule)
 qed (respectful, blast)
 
 end
